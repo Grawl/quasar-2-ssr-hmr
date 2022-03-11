337f2d9aba0e4311f61332c4a7e77140c3d1e519
Даниил Пронин <mail@grawl.ru>
2022-03-11 15:26:33 +1000

    init

---
+++ src/pages/Index.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div>
+      <div class='head'>
+        <p class='title'>Index</p>
+        <q-input v-model='input' label='input' />
+      </div>
+      <div>
+        <example-component
+          title="Example component"
+          active
+          :todos="todos"
+          :meta="meta"
+        ></example-component>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Todo, Meta } from 'components/models'
+import ExampleComponent from 'components/CompositionComponent.vue'
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { ExampleComponent },
+  setup () {
+    const todos = ref<Todo[]>([
+      {
+        id: 1,
+        content: 'ct1'
+      },
+      {
+        id: 2,
+        content: 'ct2'
+      },
+      {
+        id: 3,
+        content: 'ct3'
+      },
+      {
+        id: 4,
+        content: 'ct4'
+      },
+      {
+        id: 5,
+        content: 'ct5'
+      }
+    ])
+    const meta = ref<Meta>({
+      totalCount: 1200
+    })
+    const input = ref<string>()
+    return { todos, meta, input }
+  }
+})
+</script>
+
+<style lang='sass' scoped>
+.head
+  margin-bottom: 4em
+.title
+  font-size: 1.3em
+</style>
